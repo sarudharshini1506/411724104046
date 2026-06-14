f4121f65df012168df3148957c00f774075ea1ef
sarudharshini1506 <sarudharshini2006@gmail.com>
2026-06-14 15:46:42 +0530

    JS

---
+++ sum.js
@@ -0,0 +1,10 @@
+function findSum() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let sum = num1 + num2;
+
+    document.getElementById("result").textContent =
+        "Sum = " + sum;
+        console.log( sum);
+}
